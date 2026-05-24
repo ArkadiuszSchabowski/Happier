1a22134fbfc9a90347207630d8db2181596d4533
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2026-05-24 20:34:46 +0200

    feat: create database

---
+++ Server_Happier_Database/MyDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Server_Happier_Database.Entities;
+
+namespace Server_Happier_Database
+{
+    public class MyDbContext : DbContext
+    {
+        public DbSet<Subscribe> Subscribers { get; set; }
+
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Subscribe>(entity =>
+            {
+                entity.Property(x => x.Email)
+                    .IsRequired();
+
+                entity.HasIndex(x => x.Email)
+                    .IsUnique();
+            });
+        }
+    }
+}
