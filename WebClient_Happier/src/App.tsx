d2148e62ec34d1a5ecdc346dcad164fb194cfddd
kacper-echaust <k9echaust@gmail.com>
2026-08-17 17:20:04 +0200

    delete newsletter section

--- WebClient_Happier/src/App.tsx
+++ WebClient_Happier/src/App.tsx
@@ -2,7 +2,6 @@ import { AmbassadorsSection } from "./Components/Sections/AmbassadorsSection";
 import { EbookSection } from "./Components/Sections/EbookSection";
 import { HabitSection } from "./Components/Sections/HabitSection";
 import { DotationsSection } from "./Components/Sections/Dotations/DotationsSection";
-import { NewsletterSection } from "./Components/Sections/Newsletter/NewsletterSection";
 import { PatronitesSections } from "./Components/Sections/PatronitesSection";
 import { StartSection } from "./Components/Sections/StartSection";
 import { TestResultsSection } from "./Components/Sections/TestResultsSection";
@@ -16,7 +15,6 @@ const App = () => {
       <StartSection />
       <TestResultsSection />
       <HabitSection />
-      <NewsletterSection />
       <EbookSection />
       <DotationsSection />
       <AmbassadorsSection />
